80f2dab98eb18e69725e4a986d50767a796acddb
nellygath@gmail.com <nellygath@gmail.com>
2026-09-18 12:03:09 -0400

    Implement search service API

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -24,10 +23,11 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 //{{insert code here}}
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/gifts', giftRoutes);
+
 //{{insert code here}}
 
 
@@ -38,9 +38,11 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
+app.use('/api/gifts', giftRoutes);
 //{{insert code here}}
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+app.use('/api/search', searchRoutes);
 //{{insert code here}}
 
 
